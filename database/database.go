ca49106fc89a16a9282e94d64f0264bff6629ed3
s12kuma01 <amukat1201.ta@gmail.com>
2025-08-21 07:48:03 +0900

    add sql

---
+++ database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3" // Import the driver
+)
+
+// Connect opens a connection to the SQLite database file specified by the DSN.
+func Connect(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to the database.")
+	return db, nil
+}
+
+// Migrate runs the initial database migrations to create tables.
+func Migrate(db *sql.DB) error {
+	query := `
+	CREATE TABLE IF NOT EXISTS settings (
+		guild_id TEXT PRIMARY KEY,
+		key TEXT NOT NULL,
+		value TEXT NOT NULL
+	);
+	`
+
+	_, err := db.Exec(query)
+	return err
+}
